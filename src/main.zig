f1f636d3f7a9791d7576157bd5dc69515194c178
Crosstyan <cf595833532@gmail.com>
2024-06-29 14:01:47 +0800

    wip

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,12 @@ const usb = @cImport({
 });
 const bb = @import("bb/c.zig");
 
+const ARTO_RTOS_VID: u16 = 0x1d6b;
+const ARTO_RTOS_PID: u16 = 0x8030;
+
 /// Print the manufacturer, product, serial number of a device.
 /// If a string descriptor is not available, 'N/A' will be display.
-fn print_infos(hdl: *usb.libusb_device_handle, desc: *usb.libusb_device_descriptor) void {
+fn print_dev_info(hdl: *usb.libusb_device_handle, desc: *usb.libusb_device_descriptor) void {
     const local = struct {
         /// Get string descriptor or return a default value.
         /// Note that the caller must ensure the buffer is large enough.
@@ -84,51 +87,69 @@ pub fn main() !void {
     logz.info().fmt("libusb version", "{}.{}.{}.{}", .{ p_version.?.major, p_version.?.minor, p_version.?.micro, p_version.?.nano }).log();
     var c_device_list: [*c]?*usb.libusb_device = undefined;
     const sz = usb.libusb_get_device_list(ctx, &c_device_list);
+    // See also
+    // Device discovery and reference counting
+    // in https://libusb.sourceforge.io/api-1.0/group__libusb__dev.html
+    //
+    // https://zig.news/kprotty/resource-efficient-thread-pools-with-zig-3291
+    //
+    // If the unref_devices parameter is set, the reference count of each device
+    // in the list is decremented by 1
+    defer usb.libusb_free_device_list(c_device_list, 1);
     var device_list = c_device_list[0..@intCast(sz)];
     device_list.len = @intCast(sz);
     logz.info().int("number of device", sz).log();
-    for (device_list) |device| {
-        var desc: usb.libusb_device_descriptor = undefined;
-        ret = usb.libusb_get_device_descriptor(device, &desc);
-        if (ret != 0) {
-            continue;
-        }
-        var hdl: ?*usb.libusb_device_handle = null;
-        // zig use a unique syntax for string formatting
-        // https://github.com/ziglang/zig/blob/master/lib/std/fmt.zig
-        ret = usb.libusb_open(device, &hdl);
-        if (ret != 0) {
-            logz.warn()
-                .fmt("vid", "0x{x:0>4}", .{desc.idVendor})
-                .fmt("pid", "0x{x:0>4}", .{desc.idProduct})
-                .int("code", ret)
-                .string("what", "can't open device")
-                .log();
-            continue;
-        }
-        logz.warn()
-            .fmt("vid", "0x{x:0>4}", .{desc.idVendor})
-            .fmt("pid", "0x{x:0>4}", .{desc.idProduct})
-            .string("speed", usb_speed_to_string(usb.libusb_get_device_speed(device))).log();
-        defer usb.libusb_close(hdl);
 
-        print_infos(hdl.?, &desc);
-        const n_config = desc.bNumConfigurations;
-        for (0..n_config) |i| {
-            var p_config_: ?*usb.libusb_config_descriptor = undefined;
-            ret = usb.libusb_get_config_descriptor(device, @intCast(i), &p_config_);
-            if (ret != 0 or p_config_ == null) {
-                continue;
+    // https://www.reddit.com/r/Zig/comments/18p7w7v/making_a_struct_inside_a_function_makes_it_static/
+    // https://www.reddit.com/r/Zig/comments/ard50a/static_local_variables_in_zig/
+    const handle_device = struct {
+        pub fn call(device: *usb.libusb_device) void {
+            var lret: c_int = undefined;
+            var ldesc: usb.libusb_device_descriptor = undefined;
+            lret = usb.libusb_get_device_descriptor(device, &ldesc);
+            if (lret != 0) {
+                return;
             }
-            const p_config = p_config_.?;
-            logz.info()
-                .fmt("vid", "0x{x:0>4}", .{desc.idVendor})
-                .fmt("pid", "0x{x:0>4}", .{desc.idProduct})
-                .int("configIndex", i)
-                .int("bNumInterfaces", p_config.bNumInterfaces)
-                .int("bConfigurationValue", p_config.bConfigurationValue)
-                .int("iConfiguration", p_config.iConfiguration).log();
+            var hdl: ?*usb.libusb_device_handle = null;
+            // zig use a unique syntax for string formatting
+            // https://github.com/ziglang/zig/blob/master/lib/std/fmt.zig
+            lret = usb.libusb_open(device, &hdl);
+            if (lret != 0) {
+                logz.warn()
+                    .fmt("vid", "0x{x:0>4}", .{ldesc.idVendor})
+                    .fmt("pid", "0x{x:0>4}", .{ldesc.idProduct})
+                    .int("code", lret)
+                    .string("what", "can't open device")
+                    .log();
+                return;
+            }
+            defer usb.libusb_close(hdl);
+            logz.warn()
+                .fmt("vid", "0x{x:0>4}", .{ldesc.idVendor})
+                .fmt("pid", "0x{x:0>4}", .{ldesc.idProduct})
+                .string("speed", usb_speed_to_string(usb.libusb_get_device_speed(device))).log();
+
+            print_dev_info(hdl.?, &ldesc);
+            const n_config = ldesc.bNumConfigurations;
+            for (0..n_config) |i| {
+                var p_config_: ?*usb.libusb_config_descriptor = undefined;
+                lret = usb.libusb_get_config_descriptor(device, @intCast(i), &p_config_);
+                if (lret != 0 or p_config_ == null) {
+                    return;
+                }
+                const p_config = p_config_.?;
+                logz.info()
+                    .fmt("vid", "0x{x:0>4}", .{ldesc.idVendor})
+                    .fmt("pid", "0x{x:0>4}", .{ldesc.idProduct})
+                    .int("configIndex", i)
+                    .int("bNumInterfaces", p_config.bNumInterfaces)
+                    .int("bConfigurationValue", p_config.bConfigurationValue)
+                    .int("iConfiguration", p_config.iConfiguration).log();
+            }
         }
+    };
+    for (device_list) |device| {
+        handle_device.call(device.?);
     }
 }
 
